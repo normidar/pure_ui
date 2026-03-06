59b59b91f92ada7484829efd68b60943c42b6ffe
normidar <normidar7@gmail.com>
2026-03-06 13:18:00 +0900

    fix: address lint warning by ignoring unused element in painting.dart
    - Added a directive to ignore the unused element lint warning in the painting.dart file, improving code quality and compliance with linting rules.

--- lib/painting.dart
+++ lib/painting.dart
@@ -1,6 +1,8 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// ignore_for_file: unused_element
+
 part of dart.ui;
 
 /// Loads a single image frame from a byte array into an [Image] object.
